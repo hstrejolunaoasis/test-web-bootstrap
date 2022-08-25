521d5f76179a5d25b24a78e1eb671a1d6b56a8f5
hstrejolunaoasis <htrejo@oasishoteles.com>
2022-08-25 10:35:01 -0500

    fonts

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,42 @@
+const path = require('path')
+
 module.exports = {
-    entry: './public/app.js',
-    output: {
-      path: __dirname + '/public/dist',
-      filename: 'bundle.js'
-    },
-    module: {
-      rules: [{
-        test: /\.css$/,
-        loaders: [
-          'style-loader',
-          'css-loader'
+  entry: './src/js/main.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  devServer: {
+    static: path.resolve(__dirname, 'dist'),
+    port: 8080,
+    hot: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(scss)$/,
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader'
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins: () => [
+                  require('autoprefixer')
+                ]
+              }
+            }
+          },
+          {
+            loader: 'sass-loader'
+          }
         ]
-      }]
-    }
+      }
+    ]
   }
+}
