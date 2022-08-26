858a4246a6217230983863c58fe8fea42716ddab
hstrejolunaoasis <htrejo@oasishoteles.com>
2022-08-26 11:07:27 -0500

    medias queries

--- webpack.config.js
+++ webpack.config.js
@@ -32,10 +32,7 @@ module.exports = {
               }
             }
           },
-          {
-            test: /\.(png|svg|jpg|jpeg|gif)$/i,
-            type: 'asset/resource',
-          },
+        
           {
             loader: 'sass-loader'
           }
